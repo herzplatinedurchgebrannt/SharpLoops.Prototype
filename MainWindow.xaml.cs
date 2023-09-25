5f07b139e3f4f897a599e5d5e8d6b0f849cac7fd
herzplatinedurchgebrannt <50515453+herzplatinedurchgebrannt@users.noreply.github.com>
2023-09-25 22:25:11 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharpLoops
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void clickButton(object sender, RoutedEventArgs e)
+        {
+            ToggleButton btn = (ToggleButton)sender;
+
+            if (btn.IsChecked == true)
+            {
+                string b = btn.Name;
+                MessageBox.Show(b);
+                btn.Background = Brushes.Red;
+            }
+            else
+            {
+                btn.Background = Brushes.LightGreen;
+            }
+        }
+    }
+}
